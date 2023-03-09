143c7ce04bd14eb640e673b8b8e258125bb8c8b1
Alberto Salvador <komuk@gmail.com>
2023-03-09 13:38:00 -0600

    ADD WEB: Se obtiene un Usuario por correo y contrasenia desde el login

---
+++ web/config.js
@@ -0,0 +1,8 @@
+const SERVER = {
+    'HOST': 'http://localhost',
+    'ROOT_API': '/sysrecweb/api'
+}
+
+const API_URL = {
+    'USUARIO_CONTROLLER': SERVER.HOST + SERVER.ROOT_API + '/controllers/UsuarioController.php'
+}
---
+++ web/views/login/login.js
@@ -0,0 +1,33 @@
+const formLogin = document.getElementById('formLogin');
+
+formLogin.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    // Se obtienen los elementos del formulario.
+    const correo = document.getElementById('inputEmail');
+    const contrasenia = document.getElementById('inputPass');
+
+    // Se validan los datos del formulario.
+    if (correo.value === '' || contrasenia.value === '') {
+        return;
+    }
+
+    // // Si estan bien los datos se construte el json a enviar al servidor.
+    const data = {
+        'correo': correo.value,
+        'contrasenia': contrasenia.value
+    }
+
+    // Se realiza la peticion al servidor.
+    fetch(API_URL.USUARIO_CONTROLLER, {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(data)
+        })
+        .then(res => res.json())
+        .then(resJson => {
+            if (resJson === null) {
+                alert('Nel papi')
+            }
+        })
+});
