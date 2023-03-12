7fd0043ecd62fb55b964e3e73b47d6f711a2060d
Alberto Salvador <komuk@gmail.com>
2023-03-12 16:09:59 -0600

    FIX WEB: Sessions (Version de Hosting)

--- web/views/homeDesarrollador/homeDesarrollador.php
+++ web/views/homeDesarrollador/homeDesarrollador.php
@@ -1,3 +1,4 @@
+<?php session_start(); ?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -8,6 +9,9 @@
 </head>
 
 <body>
+    <!-- JavaScripts -->
+    <?php require_once(dirname(dirname(__FILE__)) . '/shared/scripts.php'); ?>
+    
     <!-- Sesion -->
     <?php require_once(dirname(dirname(__FILE__)) . '/shared/session/session.php'); ?>
 
@@ -16,8 +20,7 @@
 
 
 
-    <!-- JavaScripts -->
-    <?php require_once(dirname(dirname(__FILE__)) . '/shared/scripts.php'); ?>
+    
     <!-- HomeDesarrollador JavaScript -->
     <!-- <script type="text/javascript" src="homeDesarrollador.js"></script> -->
 </body>
--- web/views/shared/session/session.php
+++ web/views/shared/session/session.php
@@ -1,8 +1,6 @@
 <?php
-session_start();
 // Si no existe sesion del Usuario en el servidor, se redirecciona al Login.
 if (!isset($_SESSION['user'])) {
-    require_once(dirname(dirname(__FILE__)) . '/scripts.php');
     echo '<script type="text/javascript" src="../shared/session/session.js"></script>';
     die();
 }
